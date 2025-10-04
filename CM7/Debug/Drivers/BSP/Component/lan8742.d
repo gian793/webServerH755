9656e2425d06ad3a3c825568c909d0ecbd3e0116
gian793 <giancarlo.marcolin@gmail.com>
2025-10-04 09:34:56 +0200

    first commit

---
+++ CM7/Debug/Drivers/BSP/Component/lan8742.d
@@ -0,0 +1,4 @@
+Drivers/BSP/Component/lan8742.o: \
+ /home/gian793/Documents/Projects/webServerH755/Drivers/BSP/Components/lan8742/lan8742.c \
+ /home/gian793/Documents/Projects/webServerH755/Drivers/BSP/Components/lan8742/lan8742.h
+/home/gian793/Documents/Projects/webServerH755/Drivers/BSP/Components/lan8742/lan8742.h:
